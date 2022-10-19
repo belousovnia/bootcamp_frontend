c453f5ebd16394684e86119cf62559d41020da9a
Bogdan Dolin <d.bogdan@inbox.ru>
2022-10-19 17:34:42 +0700

    Исправил fetchCurrentUser

--- src/features/users/mocks/users.fixtures.ts
+++ src/features/users/mocks/users.fixtures.ts
@@ -1,25 +1,9 @@
 import { faker } from '@faker-js/faker';
+import { CurrentUserRoles } from '@features/auth';
 import { UserInfoFull } from '@features/users/users.entity';
 
 export const fullUserFixture: UserInfoFull = {
   id: 1,
-  surname: 'Семенов',
-  name: 'Иван',
-  patronymic: 'Иванович',
-  roles: [
-    {
-      id: faker.datatype.uuid(),
-      name: 'admin',
-    },
-  ],
-  auth: {
-    id: faker.datatype.uuid(),
-    email: 'hello@mail.com',
-  },
-  isConfirmed: true,
-  registeredAtDateMsk: '2020-01-01 00:00:00',
-  profession: {
-    id: 1,
-    name: 'Developer',
-  },
+  role: CurrentUserRoles.ROLE_REGULAR,
+  email: faker.internet.email(),
 };
