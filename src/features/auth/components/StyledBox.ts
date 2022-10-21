9474e6c2707cddfe5d683206b4b398b59b3db924
EnotelloR <uncleegor05@gmail.com>
2022-10-21 13:44:18 +0300

    Подправил вёрстку

--- src/features/auth/components/StyledBox.ts
+++ src/features/auth/components/StyledBox.ts
@@ -2,7 +2,7 @@ import { styled } from '@mui/system';
 import { Box } from '@mui/material';
 
 export const StyledBox = styled(Box)(({ theme }) => ({
-  width: '25rem',
+  width: '20rem',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
